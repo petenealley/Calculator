8e82b447a2c0e7ea1c8bbb700def21eb259a93d4
Pete Nealley <petenealley@yahoo.com>
2019-01-02 23:47:50 -0500

    Refactored Calc Buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,14 +14,36 @@ class ViewController: UIViewController {
     
     private var isFinishedTypingNumber: Bool = true
     
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else{
+                fatalError("Cannot convert label text to a Double")
+            }
+            return number
+        }
+        
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
     
         isFinishedTypingNumber = true
         
-        if sender.currentTitle == "AC" {
-            displayLabel.text = "0"
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayValue = displayValue * -1
+            }
+            else if calcMethod == "%" {
+                displayValue = displayValue / 100
+            }
+            else if sender.currentTitle == "AC" {
+                displayLabel.text = "0"
+            }
+        
         }
         
     }
@@ -37,6 +59,18 @@ class ViewController: UIViewController {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
+                
+                if numValue == "." {
+                    
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
+                
                 displayLabel.text?.append(numValue)
             }
             
